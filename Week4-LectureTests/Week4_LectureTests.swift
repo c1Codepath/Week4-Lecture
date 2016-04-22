9322930bc86dcf7bad3a15826b9ffc267e6208cc
Salyards, Adey <adey.salyards@capitalone.com>
2016-04-22 10:46:23 -0400

    Initial Commit

---
+++ Week4-LectureTests/Week4_LectureTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week4_LectureTests.swift
+//  Week4-LectureTests
+//
+//  Created by Salyards, Adey on 4/22/16.
+//  Copyright © 2016 Salyards, Adey. All rights reserved.
+//
+
+import XCTest
+@testable import Week4_Lecture
+
+class Week4_LectureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
